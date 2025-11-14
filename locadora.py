6f1377060f4b78e07f5b283869b20c928ec4d464
Pedro Humberto Priori Rodrigues <pedropriori@gmail.com>
2025-11-14 11:55:21 -0300

    add interation to the menu

--- locadora.py
+++ locadora.py
@@ -16,13 +16,13 @@ carros = [
     ("Fiat Toro", 153),
     ("Jeep Compass", 181),
     ("Volkswagen Nivus", 122)
-]
+    ]
 alugados = []
 
 
 def mostrar_lista_de_carros(lista_de_carros):
-    for i, car in enumerate(lista_de_carros):
-        print("[{}] {} - R$ {} /dia".format(i , car[0], car[1]))
+        for i, car in enumerate(lista_de_carros):
+            print("[{}] {} - R$ {} /dia".format(i , car[0], car[1]))
 
 
 
@@ -30,24 +30,45 @@ mostrar_lista_de_carros(carros)
 
 
 while True: 
-    os.system("cls")
+        os.system("cls")
 
-    print("==============================")
-    print("Bem Vindo a Locadora ByPriori") 
-    print("==============================")
-    print("Qual seus interesses")
-    print("==============================")
-    print("1 Ver tabela de carros e preços")
-    print("2 Alugar um carro")
-    print("3 Devolver um carro")
-    print("4 sair")
+        print("==============================")
+        print("Bem Vindo a Locadora ByPriori") 
+        print("==============================")
+        print("Qual seus interesses")
+        print("==============================")
+        print("1 Ver tabela de carros e preços")
+        print("2 Alugar um carro")
+        print("3 Devolver um carro")
+        op = int(input())
+        os.system("cls")
+        if op == 1:
+            print("==============================")
+            mostrar_lista_de_carros(carros)
+            print("==============================")
+        
+        elif op == 2:
+            print("==============================")
+            mostrar_lista_de_carros(carros)
+            print("==============================")
+            print("Escolha um Carro")
+            cod_car = int(input())
+            print("Por quantos dias você deseja alugar este carro?")
+            dias = int(input())
+            os.system("cls")
 
-    op = int(input())
+            print("Você escolheu {} por {} dias".format(carros[cod_car][0],dias))
+            print("O alugel Totalizaria R$ {}. Deseja Alugar ?".format(dias * carros[cod_car][1]))
 
-    os.system("cls")
-    if op == 1:
-        pass
-    elif op == 2:
-        pass 
-    elif op == 3:
-        pass 
+            print("0 - Sim | 1 - Não ")
+            resp = int(input())
+            if resp == 0:
+                print("Parabens voce alugou o {} por {} dias".format(carros[cod_car][0],dias))
+            elif op == 3:
+                    pass 
+
+        print("==============================")
+        print("Deseja continua ?")
+        print(" 0 - Continuar | 1 - Sair")
+        if float(input()) == 1 : 
+          break
